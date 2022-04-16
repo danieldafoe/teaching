33c6ac8cc66e8d209c715eb9d21726c1e9644771
Stacey39 <ching19880223@gmail.com>
2022-04-16 15:39:38 +0800

    my lab-three.component.spec.ts

--- src/app/lab-three/lab-three.component.spec.ts
+++ src/app/lab-three/lab-three.component.spec.ts
@@ -1,5 +1,33 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { LabThreeComponent } from './lab-three.component';
 
+
+
 describe('LabThreeComponent', () => {
-  
+  let component: LabThreeComponent;
+  let fixture: ComponentFixture<LabThreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      declarations: [ LabThreeComponent ],
+      providers: [FormBuilder]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LabThreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+    it ('convertToFloat', () => {
+    expect(component.convertToFloat('1.2')).toEqual(1.2);
+  });
 });
